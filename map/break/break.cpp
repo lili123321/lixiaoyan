0e31c438e00a7268759d96fc154b32a10a7cbe3c
lili123321 <37534712+lili123321@users.noreply.github.com>
2018-05-07 17:34:47 +0800

    try

--- map/break/break.cpp
+++ map/break/break.cpp
@@ -1,27 +1,27 @@
 #include<iostream>
 using namespace  std;
 int main()
-{
+{  
 	//比较break,continue,break的用法   和j++,++j的区别
-	//int  j=1;
-	//while (j!=6)
-	//{
-	//     for (int  i=0;i<=10;i++)
-	//    {
- //          if (i==6)
-	//	   {
-	//		  cout<<"i="<<"overbreak"<<endl;
-	//		  break;
- //          }
-	//	   else
-	//	   {
-	//		  cout<<"i="<<i<<endl;
-	//	   }
- //         // cout<<"里层循环"<<endl;
-	//    }
-	//	cout<<"j="<<++j<<endl;
+	int  j=1;
+	while (j!=6)
+	{
+	     for (int  i=0;i<=10;i++)
+	    {
+           if (i==6)
+		   {
+			  cout<<"i="<<"overbreak"<<endl;
+			  break;
+           }
+		   else
+		   {
+			  cout<<"i="<<i<<endl;
+		   }
+          // cout<<"里层循环"<<endl;
+	    }
+		cout<<"j="<<++j<<endl;
 
-	//}
+	}
 
 	advice();
 
